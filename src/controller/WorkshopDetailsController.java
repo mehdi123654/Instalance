baa71e1a995d329a22be146e752bff356867db72
Mehdi Fathallah <mehdi.fathallah@esprit.tn>
2023-03-09 22:38:43 +0100

    changes

--- src/controller/WorkshopDetailsController.java
+++ src/controller/WorkshopDetailsController.java
@@ -4,15 +4,13 @@
  * and open the template in the editor.
  */
 package controller;
-
 import java.net.URL;
 import java.util.ResourceBundle;
-
 import entity.Workshop;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import controller.FreelancermanagementController;
 
 /**
  * FXML Controller class
@@ -20,31 +18,35 @@ import controller.FreelancermanagementController;
  * @author fatha
  */
 public class WorkshopDetailsController implements Initializable {
-        @FXML
-        private Label workshopNameLabel;
-        @FXML
-        private Label workshopIDLabel;
-        @FXML
-        private Label workshopStartDateLabel;
-        @FXML
-        private Label workshopEndDateLabel;
-        @FXML
-        private Label workshopRegistrationDeadlineLabel;
-        @FXML
-        private Label workshopLocationLabel;
-        @FXML
-        private Label workshopMaxAttendeesLabel;
-        @FXML
-        private Label workshopAgendaLabel;
-        @FXML
-        private Label workshopDescriptionLabel;
+    @FXML
+    private Label workshopNameLabel;
+    @FXML
+    private Label workshopIDLabel;
+    @FXML
+    private Label workshopStartDateLabel;
+    @FXML
+    private Label workshopEndDateLabel;
+    @FXML
+    private Label workshopRegistrationDeadlineLabel;
+    @FXML
+    private Label workshopLocationLabel;
+    @FXML
+    private Label workshopMaxAttendeesLabel;
+    @FXML
+    private Label workshopAgendaLabel;
+    @FXML
+    private Button participate;
+    @FXML
+    private Label workshopDescriptionLabel;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
-    }    
+
+    }
+
     public void setWorkshopDetails(Workshop workshop) {
         workshopNameLabel.setText(workshop.getEvent_name());
         workshopIDLabel.setText(Integer.toString(workshop.getEvent_id()));
@@ -56,4 +58,5 @@ public class WorkshopDetailsController implements Initializable {
         workshopAgendaLabel.setText(workshop.getAgenda());
         workshopDescriptionLabel.setText(workshop.getDescription());
     }
+
 }
